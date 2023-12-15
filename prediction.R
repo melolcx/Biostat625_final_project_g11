1c9a5c20214843acd16d6ba2c74f6f996f31e549
NRBC1 <147957672+NRBC1@users.noreply.github.com>
2023-12-15 00:22:37 -0500

    Update prediction.R

--- prediction.R
+++ prediction.R
@@ -1,28 +1,31 @@
+# Input test data
 test <- read.csv("finaltest.csv")
+
+# Creat function to calculate mode
 get_mode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
-result_2 <- list()
+
 # Initialize a vector to store the final mode predictions
 final_predictions <- matrix(0,ncol=3480,nrow = 1)
 summary_predictions <- matrix(1,ncol=3480,nrow = 100)
+result_2 <- list()
 
 # Calculate the mode for each observation
 for (i in 1:100) {
-  # Extract the ith prediction from each bootstrap sample
   file <- paste0(i,".Rdata")
   load(file)
   summary_predictions[i,] <- bootstrap_predictions
 }
 
-for (i in 1:3480) {
 # Calculate the mode of these predictions
+for (i in 1:3480) {
   final_predictions[1,i] <- get_mode(summary_predictions[,i])
 }
 
+# Find confident interval
 error_predictions <- matrix(0,ncol=3480,nrow = 2)
-#confident interval
 for (i in 1:3480) {
   error_predictions[1,i] <- final_predictions[1,i]-qnorm(0.975)*sqrt(row_variances[i])
   error_predictions[2,i] <- final_predictions[1,i]+qnorm(0.975)*sqrt(row_variances[i])
@@ -34,10 +37,7 @@ for (i in 1:3480) {
     n <- n+1
   }
 }
+
+# Store the result
 a <- n/nrow(test)
-af <- a
-a_3 <- a
-
-result_2 <- c(result_2,af)
-
-#result_2 <- c(result_2,sum(final_predictions == (test$health-1))/nrow(test))
+result_2 <- c(result_2,a)
